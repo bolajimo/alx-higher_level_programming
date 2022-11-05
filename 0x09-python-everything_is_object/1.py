ab46ee0ec0242bbe2649650ecfeb5e15b2ddbee8
bolajimo <bolajimoses17@gmail.com>
2022-11-05 22:33:40 +0100

    object

---
+++ 0x09-python-everything_is_object/1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+a = 10
+b = 10
+
+print(a)
+print(b)
+
+if a == b:
+    print("true")
+    
---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_string(a=[]):
+    a.append("Holberton")
+    return ", ".join(a)
+    
