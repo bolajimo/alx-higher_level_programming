6eedf658ad847adf1354204e4df0e83dc9416884
bolajimo <bolajimoses17@gmail.com>
2023-01-16 03:44:57 +0100

    java

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const myArgs = process.argv.slice(2);
 if (myArgs[0] === undefined || isNaN(myArgs[0])) {
   console.log('Missing size');
-}; 
+} 
 else {
   for (let i = 0; i < myArgs; i++) {
     console.log('X'.repeat(myArgs));
   }
-};
+}
